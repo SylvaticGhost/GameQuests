cbf3e5c210d606e1aff74757c41632f9722df13c
Tina Topka <anon.justch1ll1n@gmail.com>
2025-02-09 22:57:13 +0200

    flex spam

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import Header from "../../components/Header.jsx";
-import AchieveCard from "../../components/AchieveCard.jsx";
 import {Avatar, Box, Divider, Button, Typography, TextField} from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 
