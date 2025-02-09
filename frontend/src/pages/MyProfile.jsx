daa850909354022bea7423ce6c5ade39ac32ff14
Tina Topka <anon.justch1ll1n@gmail.com>
2025-02-09 21:20:48 +0200

    random code go 👏👏👏

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Header from "../../components/Header.jsx";
+import {Box, Divider, Button, Typography} from "@mui/material";
 
 function MyProfile(){
-    return <></>
+    const [value, setValue] = React.useState(0);
+
+    return <Box sx={{ width: '100%' }}>
+        <Header value={value} setValue={setValue} />
+        <Typography variant="h3" fontWeight="bold">Profile</Typography>
+        <Divider>
+            <Box>
+
+            </Box>
+            <Box>
+
+            </Box>
+            <Box>
+
+            </Box>
+        </Divider>
+        <Box>
+            <Button variant="contained">Save changes</Button>
+            <Button variant="outlined">Cancel</Button>
+        </Box>
+    </Box>
 }
 
 export default MyProfile;
