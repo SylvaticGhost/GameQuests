765748644e5af7c29003bb5917c716a43dcb1418
SylvaticGhost <k.tkachov05@gmail.com>
2025-02-12 22:21:43 +0200

    add docs. delete unused code

---
+++ README.md
@@ -0,0 +1,8 @@
+# Run with docker
+### Define environment variables
+in ./backend take .env.example and rename it to .env and add credentials for Google Oauth
+### Run the following commands
+in root directory
+```bash
+docker-compose up --build
+```
--- infrastructure/read_pem.sh
+++
@@ -1,7 +0,0 @@
-PRIVATE_KEY_PEM=$(jq -r '.resources[] | select(.type == "tls_private_key") | .instances[0].attributes.private_key_pem' ./terraform.tfstate)
-
-echo -e "$PRIVATE_KEY_PEM" > h.pem
-
-chmod 600 h.pem
-
-echo "Private key written to h.pem"
